006e5f4575f12c5a519cd9de795fbdacfb69a42c
RileyStratton <riley.s.stratton@outlook.com>
2022-02-13 11:27:40 -0700

    Add project files.

---
+++ TicTacToe-cs/Practice.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello World!");
+//Console.WriteLine("I will print on a new line.");
+
+//Console.Write("Hello World! ");
+//Console.Write("I will print on the same line.");
+
+// This is a comment
+
+/* This is a
+ * multiline
+ * comment! */
+
+//string my_name = "Riley";
+//Console.WriteLine("Name: " + my_name);
+
+//char my_initial = 'R';
+//Console.WriteLine("Initial: " + my_initial);
+
+//int my_age = 22;
+//Console.WriteLine("Age: " + my_age);
+
+//bool my_life = true;
+//Console.WriteLine("Living: " + my_life);
+
+//double my_pi = 3.14152654;
+//Console.WriteLine("Pi: " + my_pi);
+
+//int x = 5, y = 6, z = 11;
+//Console.WriteLine(x + y - z);
+
+//double my_age_double = my_age;
+//Console.WriteLine("Age Double: " + my_age_double);
+
+//int my_pi_int = (int)my_pi;
+//Console.WriteLine("Pi Int: " + my_pi_int);
+
+//Console.Write("Enter your age: ");
+//int age = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Your age doubled is: " + (age * 2));
+
+string first_name = "Riley";
+string last_name = "Stratton";
+Console.WriteLine($"My full name is {first_name} {last_name}");
